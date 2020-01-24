ca1de56778c60826022ae9d8b6d376e90cbd69c6
MDalic <56225355+MDalic@users.noreply.github.com>
2020-01-24 01:54:00 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 
-const socket = io('http://localhost')
+const socket = io.connect('http://localhost');
 const messageContainer = document.getElementById('message-container')
 
 const messageForm = document.getElementById('send-container')
